881bd99546f3a5fa0be87201a494f84ba9e40e5b
rudesome <rudesome@users.noreply.github.com>
2024-09-22 10:50:59 +0000

    Updated to version v1.8.1
    Files changed:
    M	tools/chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,9 +2,10 @@ $ErrorActionPreference = 'Stop';
 $params = @{
   packageName  = 'zebar'
   FileType     = 'msi'
-  SilentArgs   = '/silent'
-  Url          = 'https://github.com/glzr-io/zebar/releases/download/v1.8.1/zebar-v1.8.1-opt1-x64.msi'
-  checksum     = '723b2bc6e19fa67d5f9f1b88ecf23b14247771e95db35897b749708e5d5e41cd'
+  SilentArgs     = "/qn /norestart /l*v ""
+  validExitCodes = @(0, 3010, 1641)
+  Url          = 'https://github.com/glzr-io/zebar/releases/download/v1.8.1/zebar-v1.8.1-opt1-x64.msi' 
+  checksum     = '723b2bc6e19fa67d5f9f1b88ecf23b14247771e95db35897b749708e5d5e41cd' 
   checksumType = 'sha256'
 }
 Install-ChocolateyPackage @params
