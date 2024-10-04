b255860edea67514598487e34d40292484ac5967
rudesome <rudesome@users.noreply.github.com>
2024-10-04 20:58:54 +0000

    Updated to version v2.3.0
    Files changed:
    M	VERSION
    M	tools/chocolateyinstall.ps1
    M	zebar.nuspec

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $params = @{
   FileType         = 'msi'
   SilentArgs       = "/qn /norestart"
   validExitCodes   = @(0, 3010, 1641)
-  Url64bit         = 'https://github.com/glzr-io/zebar/releases/download/v2.2.2/zebar-v2.2.2-opt1-x64.msi' 
-  checksum64       = '354824b120d01a6788824e1bc4c3eed1b68bddf918d8f2e9de4d6b4a1e7d88f9' 
+  Url64bit         = 'https://github.com/glzr-io/zebar/releases/download/v2.3.0/zebar-v2.3.0-opt1-x64.msi' 
+  checksum64       = 'dfde14ba8f0f0ca214d633aaa0c4bb721672f8d30ba1c9cda27e0d5fbbd4a93c' 
   checksumType64   = 'sha256'
 }
 Install-ChocolateyPackage @params
